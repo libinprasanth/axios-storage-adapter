9e444eec1d1379facbfe3d04dc62d73be1be6e95
libin <libinprasanth@gmail.com>
2023-10-14 13:14:05 +0530

    Minified the output file

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  format: ["cjs"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
-  sourcemap: true,
+  splitting: true,
+  sourcemap: false,
   clean: true,
+  minify: true
 });
